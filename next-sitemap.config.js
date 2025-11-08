483e64b246c7b80115c1f03bb968081e5d52a993
Jaymax-cyber <jossycode0@gmail.com>
2025-11-08 23:45:12 +0100

    updt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://glotofarms.com',
+  generateRobotsTxt: true, 
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
